6b9475c0b099c74ee7b5d6f2af37218ab3bcaed1
deepak9875 <deepaklakhlan1234@gmail.com>
2021-09-15 18:10:03 +0530

    search button add

--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -112,19 +112,28 @@ const Navbar = () => {
               </div>
 
               <div className="d-xl-flex align-items-center">
-                <li className="nav-item">
-                  <NavLink
-                    exact
-                    to="/"
-                    activeClassName="active"
-                    className="nav-links"
-                    onClick={click ? handleClick : null}
-                  >
-                    Home
-                  </NavLink>
+                <li className="nav-item d-xl-none ">
+                  <div>
+                    <input type="text" placeholder="Search" />
+                  </div>
                 </li>
 
-                <li className=" d-none d-xl-block">
+                <li className="nav-item d-none d-xl-block">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-search"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                  </span>
+                </li>
+
+                <li className="nav-item d-none d-md-block">
                   <NavLink
                     exact
                     to="/about"
@@ -313,7 +322,7 @@ const Navbar = () => {
                 </li>
 
                 {/* Drop down end here */}
-                <li className="nav-item d-none d-xl-block">
+                <li className="nav-item d-none d-md-block">
                   <NavLink
                     exact
                     to="/blog"
