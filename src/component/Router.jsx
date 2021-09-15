17638d245dbd4eff73eac1ac49d9fa19dc226a0a
deepak9875 <deepaklakhlan1234@gmail.com>
2021-09-15 17:35:09 +0530

    navbar done

--- src/component/Router.jsx
+++ src/component/Router.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import Homepage from "../view/Homepage";
 import OurPlanspage from "../view/OurPlanspage";
 import Stockpage from "../view/Stockpage";
+import Navbarpage from "../view/Navbarpage";
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         {/* <Route exact path="/" component={Homepage} /> */}
         {/* <Route exact path="/stock" component={Stockpage} /> */}
-        <Route exact path="/ourplans" component={OurPlanspage} />
+        {/* <Route exact path="/ourplans" component={OurPlanspage} /> */}
+        <Route exact path="/navbar" component={Navbarpage} />
       </Switch>
     </BrowserRouter>
   );
