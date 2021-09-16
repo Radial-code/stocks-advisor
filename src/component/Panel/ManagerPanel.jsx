a3084435e1f23fdc8b8a40d8c107990db1b636db
deepak9875 <deepaklakhlan1234@gmail.com>
2021-09-16 11:57:02 +0530

    sidebar done

--- src/component/Panel/ManagerPanel.jsx
+++ src/component/Panel/ManagerPanel.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Hamburgur from "../common/Hamburgur";
 import "./Panel.css";
-const ManagerPanel = () => {
+const ManagerPanel = ({ setSidebarActive, sidebarActive }) => {
   return (
     <div>
-      <Container>
-        <Row>
+      <Container className="w-100">
+        <div className="d-flex justify-content-end">
+          <div
+            onClick={() => setSidebarActive((preState) => !preState)}
+            className={`admin-overlay ${sidebarActive ? "w-100vw-m" : ""}`}
+          ></div>
+          <div className="hamburgur-none">
+            <Hamburgur setSidebarActive={setSidebarActive} />
+          </div>
+        </div>
+        <Row className="w-100">
           <Col>
-            <h1> thug life</h1>
+            <h1>thug life</h1>
           </Col>
+          <Col className="w-100"></Col>
         </Row>
       </Container>
     </div>
--- src/view/Managerpanelpage.jsx
+++ src/view/Managerpanelpage.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import SidebarPanel from "../component/common/SideBar/SidebarPanel";
 import ManagerPanel from "../component/Panel/ManagerPanel";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Managerpanelpage = () => {
+  const [sidebarActive, setSidebarActive] = useState(false);
+
   return (
     <div className="bg-f9f9f9 h-100vh">
       <Container>
         <Row>
           <Col className="d-flex">
-            <div className="w-350 mt-5 pt-5">
-              <SidebarPanel />
+            <div className=" mt-5 pt-5">
+              <SidebarPanel
+                sidebarActive={sidebarActive}
+                setSidebarActive={setSidebarActive}
+              />
             </div>
             <div className="w-100">
-              <ManagerPanel />
+              <ManagerPanel
+                sidebarActive={sidebarActive}
+                setSidebarActive={setSidebarActive}
+              />
             </div>
           </Col>
         </Row>
