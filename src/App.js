f7f204a1f81cd8b0f9425dfa736a42fe491d3257
shiv <shivc8658@gmail.com>
2021-09-17 11:31:33 +0530

    add footer and header in all :

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./assets/css/common.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Router from "./component/Router";
-
+import Navbar from "./component/common/Navbar";
+import Footer from "./component/common/Footer";
 function App() {
   return (
     <>
+      <Navbar />
       <Router />
+      <Footer />
     </>
   );
 }
