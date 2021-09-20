d39cb9b67ea3408a095c9fd3058b1968ae493fc1
deepak9875 <deepaklakhlan1234@gmail.com>
2021-09-20 12:48:02 +0530

    notification ui done

--- src/component/dashboard/Notification.jsx
+++ src/component/dashboard/Notification.jsx
@@ -6,7 +6,7 @@ import { Table, Container, Row, Col } from "react-bootstrap";
 const Notification = ({ sideBarHandler, setSidebarActive, sidebarActive }) => {
   return (
     <>
-      <Container>
+      <Container className="mr-lg-30">
         <div
           onClick={() => setSidebarActive((preState) => !preState)}
           className={`admin-overlay ${sidebarActive ? "w-100vw-m" : ""}`}
@@ -34,7 +34,7 @@ const Notification = ({ sideBarHandler, setSidebarActive, sidebarActive }) => {
               </div>
             </div>
           </Col>
-          <Col xs={12} className="">
+          <Col xs={12}>
             <section className="notification-card p-lg-5 p-2 ">
               <div className="border-b-1">
                 <p className="heading-stock fs-sm-20">Notificaion</p>
@@ -44,135 +44,287 @@ const Notification = ({ sideBarHandler, setSidebarActive, sidebarActive }) => {
                 tristique leo et pulvinar luctus. Donec aliquet mauris eget e
               </p>
 
-              <div className="mt-5">
-                <h1 className="sold-stock-text ff-popins">Sold Stocks</h1>
+              <div className="mt-5 h-calc-100vh-442 overflow-auto">
                 <div className="table-responsive sold-stock-scrollbar">
                   <table className="table table-borderless">
                     <thead>
-                      <tr className="sold-stock-table-head table-border-bottom table-border-top">
-                        <th scope="col" className="text-center">
-                          Date Bought
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
+                      <tr className="">
+                        <th scope="col" className="observations">
+                          Observations
+                        </th>
+                        <th scope="col" className="email">
+                          Push
+                        </th>
+                        <th scope="col" className="email ">
+                          Web
                         </th>
 
-                        <th scope="col" className="text-center">
-                          Date Sold
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
-                        </th>
-                        <th scope="col" className="text-center">
-                          Name
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
-                        </th>
-                        <th scope="col" className="text-center">
-                          Symbol
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
-                        </th>
-                        <th scope="col" className="text-center">
-                          Category
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
-                        </th>
-                        <th scope="col" className="text-center">
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
-                          Join Price
-                        </th>
-                        <th scope="col" className="text-center">
-                          sell Price
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
-                        </th>
-                        <th scope="col" className="text-center">
-                          Profit/Loss
-                          <span>
-                            <img
-                              className="pe-1"
-                              src={Sortarrow}
-                              alt="sort arrow"
-                            />
-                          </span>
+                        <th scope="col" className="email ">
+                          E-mail
                         </th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr className="sold-stock-data table-border-bottom">
-                        <td className="text-center">15/07/2021</td>
-                        <td className="text-center">Netflix Inc</td>
-                        <td className="text-center">NFLX</td>
-                        <td className="text-center">Portfolio</td>
-                        <td className="text-center">Common</td>
-                        <td className="text-center">$125</td>
-                        <td className="text-center">$150</td>
-                        <td className="text-center profitloss-text">25</td>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
                       </tr>
-                      <tr className="sold-stock-data table-border-bottom">
-                        <td className="text-center">15/07/2021</td>
-                        <td className="text-center">Netflix Inc</td>
-                        <td className="text-center">NFLX</td>
-                        <td className="text-center">Portfolio</td>
-                        <td className="text-center">Common</td>
-                        <td className="text-center">$125</td>
-                        <td className="text-center">$150</td>
-                        <td className="text-center first-text">10-</td>
+
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
                       </tr>
-                      <tr className="sold-stock-data table-border-bottom">
-                        <td className="text-center">15/07/2021</td>
-                        <td className="text-center">Netflix Inc</td>
-                        <td className="text-center">NFLX</td>
-                        <td className="text-center">Portfolio</td>
-                        <td className="text-center">Common</td>
-                        <td className="text-center">$125</td>
-                        <td className="text-center">$150</td>
-                        <td className="text-center profitloss-text">25</td>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
                       </tr>
-                      <tr className="sold-stock-data table-border-bottom">
-                        <td className="text-center">15/07/2021</td>
-                        <td className="text-center">Netflix Inc</td>
-                        <td className="text-center">NFLX</td>
-                        <td className="text-center">Portfolio</td>
-                        <td className="text-center">Common</td>
-                        <td className="text-center">$125</td>
-                        <td className="text-center">$150</td>
-                        <td className="text-center first-text">10-</td>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="sold-stock-data ">
+                        <td className="email">
+                          Lorem ipsum dolor sit amet, consectetur
+                        </td>
+                        <td>
+                          <label className="checkbox-notification">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+                        <td>
+                          <label className="checkbox-notification-web">
+                            <input type="checkbox" />
+                            <span class="geekmark"></span>
+                          </label>
+                        </td>
+
+                        <td>
+                          <div>
+                            <label className="checkbox-notification">
+                              <input type="checkbox" />
+                              <span class="geekmark"></span>
+                            </label>
+                          </div>
+                        </td>
                       </tr>
                     </tbody>
                   </table>
