4d9da5c4762e19bc670785d4025f6e884de972d5
Muskan <muskansaini758@gmail.com>
2021-10-08 11:31:09 +0530

    solve error in home page

--- src/component/home/HomePageSlider.jsx
+++ src/component/home/HomePageSlider.jsx
@@ -16,43 +16,49 @@ const HomePageSlider = ({ loading }) => {
     rtl: true,
   };
 
+  console.log("homeNewsList", homeNewsList);
+
   return (
     <>
       {loading ? (
         <BubblesLoader />
       ) : (
         <Slider className="hero-slider-2 w-100" {...settings}>
-          {!!homeNewsList && !!homeNewsList.length
-            ? homeNewsList.map((value, index) => {
-                return (
-                  <div key={index} className=" my-3">
-                    <img className="w-100" src={value.imagePath} alt="" />
-                    <p className="text-end stock-paragraph mt-3">
-                      <span className="d-flex justify-content-end align-items-center">
-                        <span className="d-flex justify-content-end px-2">
-                          <img className="w-19 h-19" src={img_1} alt="" />
-                        </span>
-                        <span className="cursor-pointer">
-                          {moment(value.createdAt).format("ddd/MM/yyyy")}
-                        </span>
-                      </span>
-                    </p>
-                    <h6 className="text-end heading-stock">
-                      {value.title ? value.title : "N/A"}
-                    </h6>
-                    <p className="text-end parargraph-stock pt-sm-2  ps-sm-5">
-                      {value.description}
-                    </p>
-                    <p className="text-end read-more cursor-pointer">
-                      Read More...
-                    </p>
-                    <p className="small-paragraph text-end">
-                      <span>{value.tags}</span>
-                    </p>
-                  </div>
-                );
-              })
-            : "You do not't have any news"}
+          {homeNewsList.map((value, index) => {
+            return (
+              <div key={index}>
+                <img
+                  className="w-100 slider-img"
+                  src={value.imagePath}
+                  alt=""
+                />
+                <p className="text-end stock-paragraph mt-3">
+                  {" "}
+                  <span className="d-flex justify-content-end align-items-center">
+                    {" "}
+                    <span className="d-flex justify-content-end px-2">
+                      <img className="w-19 h-19" src={img_1} alt="" />{" "}
+                    </span>{" "}
+                    <span className="cursor-pointer">
+                      {moment(value.createdAt).format("ddd/MM/yyyy")}{" "}
+                    </span>{" "}
+                  </span>{" "}
+                </p>
+                <h6 className="text-end heading-stock">
+                  {value.title ? value.title : "N/A"}{" "}
+                </h6>
+                <p className="text-end parargraph-stock pt-sm-2  ps-sm-5">
+                  {value.description}{" "}
+                </p>
+                <p className="text-end read-more cursor-pointer">
+                  Read More...{" "}
+                </p>{" "}
+                <p className="small-paragraph text-end">
+                  <span>{value.tags}</span>{" "}
+                </p>
+              </div>
+            );
+          })}
         </Slider>
       )}
     </>
