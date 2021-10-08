1bd1d1a8dc494334385db33c84aaa3cac669d9e5
Muskan <muskansaini758@gmail.com>
2021-10-08 09:20:00 +0530

    stock advisor ui

--- src/component/cmPanel/user/UserListTable.jsx
+++ src/component/cmPanel/user/UserListTable.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
+import Sortarrow from "../../../assets/img/sortarrow.png";
+
 import { useDispatch, useSelector } from "react-redux";
 import { getUserListForAdminAction } from "../../../redux/action/cmPanel/stock";
 import BubblesLoader from "../../common/BubblesLoader";
@@ -16,16 +18,37 @@ const UserListTable = () => {
   return (
     <>
       {loading ? (
-        <BubblesLoader />
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <BubblesLoader />
+        </div>
       ) : (
         <Table responsive hover className="">
-          <thead>
+          <thead className="portfolio-sticky">
             <tr className="user-list-panel">
-              <th>Join Date</th>
-              <th> Name</th>
-              <th>Phone</th>
-              <th>E-mail</th>
-              <th>Subscription Plan Name</th>
+              <th className="text-center">
+                {" "}
+                <img className="ps-1" src={Sortarrow} alt="sort arrow" />
+                Join Date{" "}
+              </th>
+              <th className="text-center">
+                {" "}
+                <img className="ps-1" src={Sortarrow} alt="sort arrow" /> Name
+              </th>
+              <th className="text-center">
+                {" "}
+                <img className="ps-1" src={Sortarrow} alt="sort arrow" />
+                Phone
+              </th>
+              <th className="text-center">
+                {" "}
+                <img className="ps-1" src={Sortarrow} alt="sort arrow" />
+                E-mail
+              </th>
+              <th className="text-center">
+                {" "}
+                <img className="ps-1" src={Sortarrow} alt="sort arrow" />
+                Subscription Plan Name
+              </th>
             </tr>
           </thead>
           <tbody className="user-details">
@@ -33,11 +56,11 @@ const UserListTable = () => {
               ? adminUserList.map(() => {
                   return (
                     <tr>
-                      <td>15/07/2021</td>
-                      <td>Jhone Doe</td>
-                      <td>1234567890</td>
-                      <td>1234567890@gmail.com</td>
-                      <td>Plan 1</td>
+                      <td className="text-center">15/07/2021</td>
+                      <td className="text-center">Jhone Doe</td>
+                      <td className="text-center">1234567890</td>
+                      <td className="text-center">1234567890@gmail.com</td>
+                      <td className="text-center">Plan 1</td>
                     </tr>
                   );
                 })
