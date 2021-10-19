4345bc66427b6c829a5c46e4006b567945947079
Muskan <muskansaini758@gmail.com>
2021-10-19 10:41:27 +0530

    add new notification page

--- src/component/cmPanel/user/AdminNotification.jsx
+++ src/component/cmPanel/user/AdminNotification.jsx
@@ -1,24 +1,15 @@
 import React from "react";
-import { Col, Form, FormGroup } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import Notification from "./Notification";
+
 function AdminNotification() {
   return (
-    <div className="conatiner">
-      <div className="row">
-        <div className="col-12">
-          <div className="manger-panel-shadow p-sm-5 w-xl-1000 ">
-            <div className="row">
-              <div className="col-12 col-lg-6 mb-3">
-                <Form.Group
-                  className="mb-3 add-new-stock-field "
-                  controlId="formBasicEmail"
-                >
-                  <Form.Control type="text" placeholder="Arabic Title" />
-                </Form.Group>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Container>
+        <Row>
+          <Notification />
+        </Row>
+      </Container>
     </div>
   );
 }
