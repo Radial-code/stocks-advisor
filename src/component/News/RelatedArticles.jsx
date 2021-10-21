6e64d9c2e0903b83eb96779a4d6e4563549024ec
Muskan <muskansaini758@gmail.com>
2021-10-21 16:26:53 +0530

    add spme section in news carousel

--- src/component/News/RelatedArticles.jsx
+++ src/component/News/RelatedArticles.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import CalenderIcon from "../../assets/img/calendar-icon.png";
+
 import { useDispatch, useSelector } from "react-redux";
 import { getRelatedNewsAction } from "../../redux/action/news";
 import BubblesLoader from "../common/BubblesLoader";
+import moment from "moment";
 
 const RelatedArticles = () => {
   const dispatch = useDispatch();
@@ -41,7 +44,7 @@ const RelatedArticles = () => {
                       lg={6}
                       className="d-flex justify-content-center"
                     >
-                      <section key={index} className="articles-card ">
+                      <section key={index} className="articles-card p-3">
                         <div className="related-articles-img">
                           <img
                             className="w-100 h-100"
@@ -49,6 +52,20 @@ const RelatedArticles = () => {
                             alt=""
                           />
                         </div>
+                        <p className="text-end stock-paragraph cursor-pointer">
+                          <span>
+                            {" "}
+                            {moment(value.createdAt).format("DD/MMM/YYYY")}
+                          </span>
+                          <img
+                            className="w-19 h-19 d-inline-block mx-2"
+                            src={CalenderIcon}
+                            alt="CalenderIcon"
+                          />
+                        </p>
+                        <span className="text-ellipsis-three-line stock-paragraph text-end">
+                          {value.description}
+                        </span>
                         <div className="d-flex justify-content-center align-items-center mt-4">
                           <p
                             className="news-heading-font max-w-articles p-2 Slider-Ellipse"
@@ -57,6 +74,19 @@ const RelatedArticles = () => {
                             {value.title}
                           </p>
                         </div>
+
+                        <span className="read-more ">Read More...</span>
+                        <p className="text-end cursor-pointer mb-0 text-ellipsis-dots">
+                          {value.tags}
+                        </p>
+                        <p className="small-paragraph text-end">
+                          <span className=" cursor-pointer fw-bold">
+                            Stock :
+                          </span>
+                          <span className="cursor-pointer fw-bold">
+                            {value.stock.symbol}
+                          </span>
+                        </p>
                       </section>
                     </Col>
                   );
