5a08706d26d055d21f17647dfd01c16cfae53663
Muskan <muskansaini758@gmail.com>
2021-10-22 17:19:10 +0530

    solve issues

--- src/component/common/ourPlans/OurPlans.jsx
+++ src/component/common/ourPlans/OurPlans.jsx
@@ -1,3 +1,99 @@
+// import React, { useEffect, useState } from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import ReactPaginate from "react-paginate";
+// import { useDispatch } from "react-redux";
+// import { withRouter } from "react-router";
+// import { getPlansListAction } from "../../../redux/action/cmPanel/plans";
+// import { useLayoutChangerProvider } from "../../../redux/LayoutChangerProvider";
+// import BubblesLoader from "../BubblesLoader";
+// import OurPlanCard from "./OurPlanCard";
+
+// const OurPlans = ({ homepage, history }) => {
+//   const dispatch = useDispatch();
+
+//   const [loading, setLoading] = useState(false);
+//   const [page, setPage] = useState(0);
+//   const [planListCount, setPlanListCount] = useState(0);
+//   const { setLayoutClickChanger, layoutClickChanger } =
+//     useLayoutChangerProvider();
+
+//   useEffect(() => {
+//     const limit = 3;
+//     dispatch(getPlansListAction(setLoading, page, limit, setPlanListCount));
+//   }, [page]);
+
+//   const handlePageClick = (e) => {
+//     const selectedPage = e.selected;
+//     setPage(selectedPage);
+//   };
+
+//   return (
+//     <>
+//       <Container>
+//         <Row>
+//           <Col xs={12} className="d-flex  justify-content-center">
+//             <p
+//               className={`subscription-plans mt-sm-5 mt-3 ${
+//                 homepage ? "text-white" : ""
+//               } `}
+//             >
+//               Subscription Plans
+//             </p>
+//           </Col>
+//           {loading ? (
+//             <div className="text-center d-flex  justify-content-center pt-2 mx-4">
+//               <BubblesLoader />
+//             </div>
+//           ) : (
+//             <OurPlanCard history={history} />
+//           )}
+//           {planListCount && planListCount <= 10 ? (
+//             ""
+//           ) : (
+//             <>
+//               {layoutClickChanger ? (
+//                 <div className="mx-xxl-5 mx-xl-3  d-flex justify-content-md-start justify-content-center">
+//                   <ReactPaginate
+//                     previousLabel={"Prev"}
+//                     nextLabel={"Next"}
+//                     breakLabel={"..."}
+//                     breakClassName={"break-me"}
+//                     pageCount={Math.ceil(planListCount / 3)}
+//                     marginPagesDisplayed={3}
+//                     pageRangeDisplayed={3}
+//                     onPageChange={handlePageClick}
+//                     containerClassName={"pagination"}
+//                     subContainerClassName={"pages pagination"}
+//                     activeClassName={"activePage"}
+//                     initialPage={page}
+//                   />
+//                 </div>
+//               ) : (
+//                 <div className="react-pagination mx-xxl-4 mx-xl-3 d-flex justify-content-md-end justify-content-center">
+//                   <ReactPaginate
+//                     previousLabel={"Prev"}
+//                     nextLabel={"Next"}
+//                     breakLabel={"..."}
+//                     breakClassName={"break-me"}
+//                     pageCount={Math.ceil(planListCount / 3)}
+//                     marginPagesDisplayed={3}
+//                     pageRangeDisplayed={3}
+//                     onPageChange={handlePageClick}
+//                     containerClassName={"pagination"}
+//                     subContainerClassName={"pages pagination"}
+//                     activeClassName={"activePage"}
+//                     initialPage={page}
+//                   />
+//                 </div>
+//               )}
+//             </>
+//           )}
+//         </Row>
+//       </Container>
+//     </>
+//   );
+// };
+// export default withRouter(OurPlans);
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ReactPaginate from "react-paginate";
@@ -7,26 +103,21 @@ import { getPlansListAction } from "../../../redux/action/cmPanel/plans";
 import { useLayoutChangerProvider } from "../../../redux/LayoutChangerProvider";
 import BubblesLoader from "../BubblesLoader";
 import OurPlanCard from "./OurPlanCard";
-
 const OurPlans = ({ homepage, history }) => {
   const dispatch = useDispatch();
-
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(0);
   const [planListCount, setPlanListCount] = useState(0);
   const { setLayoutClickChanger, layoutClickChanger } =
     useLayoutChangerProvider();
-
   useEffect(() => {
     const limit = 3;
     dispatch(getPlansListAction(setLoading, page, limit, setPlanListCount));
   }, [page]);
-
   const handlePageClick = (e) => {
     const selectedPage = e.selected;
     setPage(selectedPage);
   };
-
   return (
     <>
       <Container>
@@ -69,7 +160,7 @@ const OurPlans = ({ homepage, history }) => {
                   />
                 </div>
               ) : (
-                <div className="react-pagination mx-xxl-4 mx-xl-3 d-flex justify-content-md-end justify-content-center">
+                <div className="react-pagination mx-xxl-4 mx-xl-3 d-flex justify-content-md-start justify-content-center">
                   <ReactPaginate
                     previousLabel={"Prev"}
                     nextLabel={"Next"}
