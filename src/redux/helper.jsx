052b588bf3d9d3e4048388b939639c4060b30471
palvi tiwari <palvitiwari913@gmail.com>
2021-10-26 16:28:09 +0530

    update main with live link

--- src/redux/helper.jsx
+++ src/redux/helper.jsx
@@ -3,6 +3,7 @@ import Axios from "axios";
 // export let SERVER_URL = "https://a0ac-103-240-235-63.ngrok.io";
 export let SERVER_URL = "https://stockadvisor-api.herokuapp.com";
 
+//main
 export let PUBLIC_URL = `${SERVER_URL}/api/v1`;
 
 export async function axiosRequest(method, url, data = null) {
