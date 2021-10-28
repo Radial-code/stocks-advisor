2d9b5489361766abc8f38fac56bfd2521489b615
palvi tiwari <palvitiwari913@gmail.com>
2021-10-28 09:14:04 +0530

    issuess

--- src/redux/helper.jsx
+++ src/redux/helper.jsx
@@ -1,5 +1,4 @@
 import Axios from "axios";
-// export let SERVER_URL = "https://7967-103-21-184-219.ngrok.io";
 // export let SERVER_URL = "https://a0ac-103-240-235-63.ngrok.io";
 export let SERVER_URL = "https://stockadvisor-api.herokuapp.com";
 
