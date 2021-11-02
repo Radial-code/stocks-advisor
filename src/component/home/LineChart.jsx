5b5936dabe9b631da70ccd8d764523dac50819da
palvi tiwari <palvitiwari913@gmail.com>
2021-11-02 14:31:06 +0530

    update live server

--- src/component/home/LineChart.jsx
+++ src/component/home/LineChart.jsx
@@ -18,14 +18,31 @@ function LineChart({ chatValue }) {
     chatValue.values &&
     chatValue.values.map((value) => {
       if (value.profitOrLoss !== null) {
-        data.push({ Year: value.year, Profit: `${value.profitOrLoss}%` });
+        data.push({ Year: value.year, Profit: value.profitOrLoss });
       }
     });
-  console.log(data);
+
+  const CustomTooltip = ({ active, payload }) => {
+    for (const bar of payload)
+      return (
+        <div className="bg-white p-3">
+          <p className="text-gray-700">
+            {layoutClickChanger
+              ? `${bar && bar.payload && bar.payload.Year} : ${"Year"}`
+              : `${"Year"} : ${bar && bar.payload && bar.payload.Year}`}
+            {bar && bar.payload && bar.payload.data && bar.payload.data.Profit}
+          </p>
+          {bar && bar.name} To: {bar && bar.value.toFixed(2)}%
+        </div>
+      );
+
+    return null;
+  };
+
   return (
     <div className="container px-sm-4 px-1  mt-2">
       <p className="chart-text-2 ff-popins mb-1">
-        {getValueOf(chatValue.title)} {getValueOf("Portfolio")}
+        {chatValue.title} {getValueOf("Portfolio")}
       </p>
       <div
         className={`${
@@ -39,6 +56,7 @@ function LineChart({ chatValue }) {
         <ResponsiveContainer width="100%" height={213}>
           <AreaChart
             data={data}
+            legend={false}
             margin={{
               top: 10,
               right: 20,
@@ -55,27 +73,30 @@ function LineChart({ chatValue }) {
 
             <XAxis dataKey="Year" axisLine={false} tickLine={false} />
             <YAxis
-              dataKey={getValueOf("Profit To")}
+              dataKey="Profit"
               dx={layoutClickChanger ? -30 : -10}
-              axisLine={false}
-              tickLine={false}
+              axisLine={true}
+              tickLine={true}
               tickFormatter={(number) => `${number}%`}
             />
-            <Tooltip />
+            <Tooltip
+              content={<CustomTooltip />}
+              labelFormatter={(value) => `${value}`}
+            />
             <Area
-              dataKey={getValueOf("Profit To")}
+              dataKey="Profit"
               stroke={`${
-                chatValue.title === `${getValueOf("Long Term")}`
+                chatValue.title === "Long Term"
                   ? "#5CBD4C"
-                  : chatValue.title === `${getValueOf("Trading")}`
+                  : chatValue.title === "Trading"
                   ? "#4870B3"
                   : "#BD4C67"
               }`}
               strokeWidth="2"
               fill={`${
-                chatValue.title === `${getValueOf("Long Term")}`
+                chatValue.title === "Long Term"
                   ? "#5CBD4C"
-                  : chatValue.title === `${getValueOf("Trading")}`
+                  : chatValue.title === "Trading"
                   ? "#4870B3"
                   : "#BD4C67"
               }`}
