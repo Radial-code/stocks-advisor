14444b0c99e22a74e97266409763bf5f5eeb0763
Muskan <muskansaini758@gmail.com>
2021-11-02 12:46:30 +0530

    add ab json and en json file

--- src/component/common/ourPlans/OurPlanCard.jsx
+++ src/component/common/ourPlans/OurPlanCard.jsx
@@ -14,7 +14,7 @@ const OurPlanCard = ({ homepage, history }) => {
   const pathname = window.location.pathname;
   var settings = {
     dots: false,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -26,7 +26,6 @@ const OurPlanCard = ({ homepage, history }) => {
         breakpoint: 1400,
         settings: {
           slidesToShow: 2,
-
           dots: true,
         },
       },
